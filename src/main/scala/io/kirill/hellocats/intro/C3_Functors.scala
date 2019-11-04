4d1c06479fcf885ba1892bd22f0af9e3e8f67679
kirill5k <immotional@aol.com>
2019-11-04 19:12:35 +0000

    contramap and imap

--- src/main/scala/io/kirill/hellocats/intro/C3_Functors.scala
+++ src/main/scala/io/kirill/hellocats/intro/C3_Functors.scala
@@ -41,4 +41,53 @@ object Functors extends App {
       case Branch(left, right) => Branch(map(left)(f), map(right)(f))
     }
   }
+
+  trait Printable[A] { self =>
+    def format(value: A): String
+    def contramap[B](f: B => A): Printable[B] = new Printable[B] {
+      override def format(value: B): String = self.format(f(value))
+    }
+  }
+
+  def format[A](value: A)(implicit p: Printable[A]): String = p.format(value)
+
+  implicit val stringPrintable: Printable[String] = (value: String) => "\"" + value +  "\""
+  implicit val booleanPrintable: Printable[Boolean] = (value: Boolean) => if (value) "yes" else "no"
+
+  println(format("hello"))
+  println(format(true))
+
+  final case class Box[A](value: A)
+  implicit def boxPrintable[A](implicit p: Printable[A]): Printable[Box[A]] = p.contramap(_.value)
+
+  println(format(Box(false)))
+
+  trait Codec[A] { self =>
+    def encode(value: A): String
+    def decode(value: String): A
+    def imap[B](dec: A => B, enc: B => A): Codec[B] = new Codec[B] {
+      override def encode(value: B): String = self.encode(enc(value))
+      override def decode(value: String): B = dec(self.decode(value))
+    }
+  }
+
+  def encode[A](value: A)(implicit c: Codec[A]): String = c.encode(value)
+  def decode[A](value: String)(implicit c: Codec[A]): A = c.decode(value)
+
+  implicit val stringCodec: Codec[String] = new Codec[String] {
+    override def encode(value: String): String = value
+    override def decode(value: String): String = value
+  }
+
+  implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
+  implicit val booleanCodec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+  implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+  implicit def boxCodec[A](implicit c: Codec[A]): Codec[Box[A]] = stringCodec.imap(c.decode, c.encode)
+
+  import cats.Contravariant
+  import cats.Show
+  import cats.instances.string._
+
+  val showString = Show[String]
+  val showSymbol = Contravariant[Show].contramap(showString)((sym: Symbol) => s"'${sym.name}")
 }
