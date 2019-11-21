e921ec87973353d7da8d9e318f31b8b5f488b72d
kirill5k <immotional@aol.com>
2019-11-21 19:35:44 +0000

    monad composition

--- src/main/scala/io/kirill/hellocats/monads/C5_Compositions.scala
+++ src/main/scala/io/kirill/hellocats/monads/C5_Compositions.scala
@@ -4,59 +4,86 @@ import cats.data.EitherT
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.postfixOps
 
 object C5_Compositions extends App {
 
   import cats.Monad
-  import cats.syntax.applicative._
-  import cats.syntax.flatMap._
-  import cats.instances.list._
-  import cats.instances.either._
-  import cats.instances.future._
-  import cats.data.OptionT
-  import scala.concurrent.ExecutionContext
-  implicit val executor: ExecutionContext =  scala.concurrent.ExecutionContext.global
+  import cats.data.{OptionT, EitherT, Writer} // EitherT, ReaderT, WriterT, StateT, IdT
+  import cats.instances.list._ // for Monad
+  import cats.instances.future._ // for Monad
+  import cats.instances.either._ // for Monad
+  import cats.syntax.applicative._ // for pure
+  import cats.syntax.either._ // for pure
+  import cats.syntax.option._
 
-  type ListOption[A] = OptionT[List, A]
+  type ListOption[A] = OptionT[List, A] // List[Option[A]] transform
 
-  val a1: ListOption[Int] = OptionT(List(Option(10)))
-  val b1: ListOption[Int] = 32.pure[ListOption]
+  val res1: ListOption[Int] = OptionT(List(10.some))
+  val res2: ListOption[Int] = 32.pure[ListOption] // List(Some(32))
+  val res3 = res1.flatMap(x => res2.map(y => x +y))
+  println(res3)
 
-  val c1 = a1.flatMap(x => b1.map(y => x+y))
-  println(c1)
 
   type ErrorOr[A] = Either[String, A]
   type ErrorOrOption[A] = OptionT[ErrorOr, A]
+  val a = 10.pure[ErrorOrOption]
+  val b = 32.pure[ErrorOrOption]
+  val c = a.flatMap(x => b.map(y => x + y))
+  println(c)
 
-  val a2 = 10.pure[ErrorOrOption]
-  val b2 = 32.pure[ErrorOrOption]
-  val c2 = a2.flatMap(x => b2.map(y => x+y))
-  println(c2) // OptionT(Right(Some(42)))
-  println(a2.value)
+
+  sealed abstract class HttpError
+  final case class NotFound(item: String) extends HttpError
+  final case class BadRequest(msg: String) extends HttpError
+  type FutureEither[A] = EitherT[Future, HttpError, A]
+
+
+  type Logged[A] = Writer[List[String], A]
+  def parseNumber(str: String): Logged[Option[Int]] =
+    util.Try(str.toInt).toOption match {
+      case Some(num) => Writer(List(s"read $str"), Some(num))
+      case None => Writer(List(s"failed on $str"), None)
+    }
+
+  def addAll(a: String, b: String, c: String): Logged[Option[Int]] = {
+    val result = for {
+      x <- OptionT(parseNumber(a))
+      y <- OptionT(parseNumber(b))
+      z <- OptionT(parseNumber(c))
+    } yield x + y + z
+    result.value
+  }
+  println(addAll("1", "2", "3"))
+  println(addAll("1", "a", "3"))
+
 
 
   type Response[A] = EitherT[Future, String, A]
-
-  val powerLevels = Map("jazz" -> 6, "bumblebee" -> 8, "hot rod" -> 10)
-
-  def getPowerLevel(autobot: String): Response[Int] = {
+  val powerLevels = Map("jazz" -> 6, "bumblebee" -> 8, "hot-rod" -> 10)
+  def getPowerLevel(autobot: String): Response[Int] =
     powerLevels.get(autobot) match {
-      case Some(value) => EitherT.right(Future(value))
-      case None => EitherT.left(Future("not found"))
+      case Some(powerlevel) => EitherT.right(Future(powerlevel))
+      case None => EitherT.left(Future(s"couldnt find powerlevel of $autobot"))
     }
-  }
 
   def canSpecialMove(ally1: String, ally2: String): Response[Boolean] = {
-    getPowerLevel(ally1).flatMap(p1 => getPowerLevel(ally2).map(p2 => (p1 + p2) > 15))
+    for {
+      a1 <- getPowerLevel(ally1)
+      a2 <- getPowerLevel(ally2)
+    } yield (a1 + a2) > 15
   }
 
-  def tactialRepost(ally1: String, ally2: String): String = {
+  def tacticalReport(ally1: String, ally2: String): String = {
     val stack = canSpecialMove(ally1, ally2).value
-    Await.result(stack, 2 seconds) match {
+    Await.result(stack, 1 second) match {
       case Left(msg) => s"comms error: $msg"
-      case Right(true) => s"$ally1 and $ally2 can work together"
-      case Right(false) => s"$ally1 and $ally2 are no good"
+      case Right(true) => s"$ally1 and $ally2 are good"
+      case Right(false) => s"$ally1 and $ally2 are not strong enought"
     }
   }
+
+  println(tacticalReport("hot-rod", "jazz"))
+  println(tacticalReport("hot-rod", "optimus"))
 }
