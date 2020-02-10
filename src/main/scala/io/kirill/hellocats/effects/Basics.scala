b071045253a6a7ff64297e83937424205e598f09
kirill <immotional@aol.com>
2020-02-10 08:24:34 +0000

    effect basics

--- src/main/scala/io/kirill/hellocats/effects/Basics.scala
+++ src/main/scala/io/kirill/hellocats/effects/Basics.scala
@@ -22,4 +22,22 @@ object Basics extends App {
       inStream <- inputStream(in)
       outStream <- outputStream(out)
     } yield (inStream, outStream)
+
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+    for {
+      amount <- IO(origin.read(buffer, 0, buffer.size))
+      count  <- if(amount > -1) IO(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+                else IO.pure(acc)
+    } yield count
+
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+    for {
+      buffer <- IO(new Array[Byte](1024 * 10))
+      total  <- transmit(origin, destination, buffer, 0L)
+    } yield total
+
+  def copy(origin: File, destination: File): IO[Long] =
+    inputOutputStream(origin, destination).use { case(in, out) =>
+      transfer(in, out)
+    }
 }
