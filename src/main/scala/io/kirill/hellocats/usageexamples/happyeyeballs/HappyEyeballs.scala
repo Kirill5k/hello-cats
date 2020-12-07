40c14d690b1cdc9d2ae2d8a9a86fddbd48e6c1c2
kirill5k <immotional@aol.com>
2020-12-07 20:32:25 +0000

    happy eyeballs

--- src/main/scala/io/kirill/hellocats/usageexamples/happyeyeballs/HappyEyeballs.scala
+++ src/main/scala/io/kirill/hellocats/usageexamples/happyeyeballs/HappyEyeballs.scala
@@ -1,5 +1,6 @@
 package io.kirill.hellocats.usageexamples.happyeyeballs
 
+import cats.effect.concurrent.Deferred
 import cats.effect.{Concurrent, Timer}
 import cats.implicits._
 
@@ -7,16 +8,20 @@ import scala.concurrent.duration.FiniteDuration
 
 object HappyEyeballs {
 
-  def basic[F[_]: Concurrent: Timer](effects: List[F[Unit]], delay: FiniteDuration): F[Unit] =
-    effects match {
+  def run[F[_]: Concurrent: Timer, A](tasks: List[F[A]], delay: FiniteDuration): F[A] =
+    tasks match {
       case Nil =>
         Concurrent[F].raiseError(new IllegalArgumentException(""))
       case head :: Nil =>
         head
       case head :: tail =>
-        Concurrent[F].race(head, Timer[F].sleep(delay) *> basic(tail, delay)).map { _ =>
-          ()
+        Deferred[F, Unit].flatMap { errorTrigger =>
+          val task1 = head.handleErrorWith(e => errorTrigger.complete(()) *> Concurrent[F].raiseError(e))
+          val waitForDelayOrError = Concurrent[F].race(Timer[F].sleep(delay), errorTrigger.get)
+          Concurrent[F].race(task1, waitForDelayOrError *> run(tail, delay)).map {
+            case Left(res) => res
+            case Right(res) => res
+          }
         }
-
     }
 }
