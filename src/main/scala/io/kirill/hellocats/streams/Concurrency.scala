9c915b097ca44392e60a916642e3398b1c1be9dc
kirill5k <immotional@aol.com>
2020-12-08 19:56:13 +0000

    fs2 - stop after

---
+++ src/main/scala/io/kirill/hellocats/streams/Concurrency.scala
@@ -0,0 +1,34 @@
+package io.kirill.hellocats.streams
+
+import cats.effect.{Concurrent, Timer}
+import fs2.Stream
+import fs2.concurrent.{Signal, SignallingRef}
+
+import scala.concurrent.duration.FiniteDuration
+
+object Concurrency {
+
+  def stopAfter[F[_]: Concurrent: Timer, A](in: Stream[F, A], delay: FiniteDuration): Stream[F, A] = {
+    def out(interrupter: SignallingRef[F, Boolean]): Stream[F, A] =
+      in.interruptWhen(interrupter)
+
+    def stop(interrupter: SignallingRef[F, Boolean]): Stream[F, Unit] =
+      Stream.sleep_[F](delay) ++ Stream.eval(interrupter.set(true))
+
+    Stream.eval(SignallingRef[F, Boolean](false)).flatMap { interrupter =>
+      val stopper = stop(interrupter)
+      val runner  = out(interrupter)
+      runner.concurrently(stopper)
+    }
+  }
+
+  def stopAfterPipe[F[_]: Concurrent: Timer, A](delay: FiniteDuration): Stream[F, A] => Stream[F, A] = in => {
+    def close(interrupter: SignallingRef[F, Boolean]): Stream[F, Unit] =
+      Stream.sleep_[F](delay) ++ Stream.eval(interrupter.set(true))
+
+    Stream.eval(SignallingRef[F, Boolean](false)).flatMap { interrupter =>
+      in.interruptWhen(interrupter).concurrently(close(interrupter))
+    }
+
+  }
+}
