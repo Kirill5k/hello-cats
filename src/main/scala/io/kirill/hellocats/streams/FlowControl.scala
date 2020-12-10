574702f4eca45120e4f96ed7425792147d9243d4
kirill5k <immotional@aol.com>
2020-12-10 14:33:21 +0000

    another function example

--- src/main/scala/io/kirill/hellocats/streams/FlowControl.scala
+++ src/main/scala/io/kirill/hellocats/streams/FlowControl.scala
@@ -3,7 +3,7 @@ package io.kirill.hellocats.streams
 import cats.effect.concurrent.MVar
 import cats.effect.{Concurrent, Timer}
 import cats.implicits._
-import fs2.Stream
+import fs2.{Pipe, Stream}
 import fs2.concurrent.SignallingRef
 
 import scala.concurrent.duration._
@@ -22,7 +22,7 @@ object FlowControl {
     }
   }
 
-  def stopAfterPipe[F[_]: Concurrent: Timer, A](delay: FiniteDuration): Stream[F, A] => Stream[F, A] = in => {
+  def stopAfterPipe[F[_]: Concurrent: Timer, A](delay: FiniteDuration): Pipe[F, A, A] = in => {
     def close(interrupter: SignallingRef[F, Boolean]): Stream[F, Unit] =
       Stream.sleep_[F](delay) ++ Stream.eval(interrupter.set(true))
 
@@ -49,4 +49,14 @@ object FlowControl {
       delaysUntilReset.repeat.concurrently(resets.evalMap(_ => restart.put(()) *> resetting))
     }
   }
+
+  def tk[F[_],O](n: Long): Pipe[F,O,O] =
+    in => in.scanChunksOpt(n) { n =>
+      if (n <= 0) None
+      else Some(c => c.size match {
+        case m if m < n => (n - m, c)
+        case m => (0, c.take(n.toInt))
+      })
+    }
+
 }
