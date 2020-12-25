861639f6b80d59ddb57bf9fa6ca516efb54c630c
kirill5k <immotional@aol.com>
2020-12-25 16:00:54 +0000

    final refactor

--- src/main/scala/io/kirill/hellocats/streams/WebAuth.scala
+++ src/main/scala/io/kirill/hellocats/streams/WebAuth.scala
@@ -25,10 +25,10 @@ object WebAuth extends IOApp {
       Timer[F].sleep(200.millis) *>
       Sync[F].delay(AuthToken(UUID.randomUUID().toString, 5000))
 
-  def authStream[F[_]: Sync: Timer]: Stream[F, String] =
+  def authStream[F[_]: Sync: Timer](authToken: SignallingRef[F, AuthToken]): Stream[F, Unit] =
     Stream
-      .eval(authenticate)
-      .flatMap(t => Stream(t.token).covary[F] ++ Stream.sleep_[F](t.expiresIn.millis))
+      .eval(authToken.get)
+      .flatMap(t => Stream.sleep_[F](t.expiresIn.millis) ++ Stream.eval(authenticate).evalMap(authToken.set))
       .repeat
 
   def getItem[F[_]: Sync: Timer](token: String, page: Int): F[ItemsResponse] =
@@ -38,27 +38,24 @@ object WebAuth extends IOApp {
         ItemsResponse(
           List(Item(UUID.randomUUID().toString)),
           page,
-          if (page < 10) Some(page + 1) else None
+          if (page < 25) Some(page + 1) else None
         )
       )
 
-  def itemStream[F[_]: Sync: Timer](authToken: SignallingRef[F, String]): Stream[F, Item] =
+  def itemStream[F[_]: Sync: Timer](authToken: SignallingRef[F, AuthToken]): Stream[F, Item] =
     Stream
       .unfoldEval[F, Option[Int], List[Item]](Some(0)) { currentPage =>
         currentPage.traverse { page =>
-          authToken.get.flatMap(t => getItem(t, page).map(r => (r.items, r.nextPage)))
+          authToken.get.flatMap(t => getItem(t.token, page).map(r => (r.items, r.nextPage)))
         }
       }
       .flatMap(Stream.emits)
-      .repeat
 
   override def run(args: List[String]): IO[ExitCode] =
     Stream
-      .eval(authenticate[IO].flatMap(t => SignallingRef[IO, String](t.token)))
+      .eval(authenticate[IO].flatMap(SignallingRef[IO, AuthToken]))
       .flatMap { token =>
-        val tokens = authStream[IO].evalMap(newToken => token.set(newToken)).drain
-        val items  = itemStream[IO](token)
-        tokens.merge(items)
+        authStream[IO](token).mergeHaltBoth(itemStream[IO](token))
       }
       .compile
       .drain
