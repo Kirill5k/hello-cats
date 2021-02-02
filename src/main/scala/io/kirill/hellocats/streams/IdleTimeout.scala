54661c1ec80802b677aee508c806ee1b4dc64f9b
kirill5k <immotional@aol.com>
2021-02-02 17:17:07 +0000

    idle timeout

--- src/main/scala/io/kirill/hellocats/streams/IdleTimeout.scala
+++ src/main/scala/io/kirill/hellocats/streams/IdleTimeout.scala
@@ -2,22 +2,29 @@ package io.kirill.hellocats.streams
 
 import cats.effect.{Concurrent, ExitCode, IO, IOApp, Timer}
 import fs2.Stream
+import io.kirill.hellocats.utils.printing._
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
 
 object IdleTimeout extends IOApp {
 
-  def idleTimeout[F[_], A](
+  def idleTimeout[F[_]: Timer, A](
       s: fs2.Stream[F, A],
       timeout: FiniteDuration
-  )(implicit
-      F: Concurrent[F],
-      timer: Timer[F]
-  ): fs2.Stream[F, A] =
-    s.groupWithin(1, timeout)
-      .evalMap(
-        _.head.fold[F[A]](F.raiseError(new Exception("timeout")))(F.pure)
-      )
+  )(implicit F: Concurrent[F]): fs2.Stream[F, A] =
+    s
+      .groupWithin(1, timeout)
+      .evalMap(_.head.fold[F[A]](F.raiseError(new Exception("timeout")))(F.pure))
 
-  override def run(args: List[String]): IO[ExitCode] = ???
+  override def run(args: List[String]): IO[ExitCode] = {
+    val s = Stream
+      .range[IO](0, 100)
+      .evalTap(i => putStr[IO](i.toString))
+      .metered(1.second)
+    idleTimeout[IO, Int](s, 100.millis)
+      .handleErrorWith(e => Stream.eval_(putStr[IO](e.getMessage)))
+      .compile
+      .drain
+      .as(ExitCode.Success)
+  }
 }
