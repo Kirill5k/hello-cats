a0c6d8c37675c4182b9c60c19f70a3971568941d
kirill5k <immotional@aol.com>
2021-02-02 20:48:03 +0000

    idleTimeout refactor

--- src/main/scala/io/kirill/hellocats/streams/IdleTimeout.scala
+++ src/main/scala/io/kirill/hellocats/streams/IdleTimeout.scala
@@ -1,30 +1,45 @@
 package io.kirill.hellocats.streams
 
-import cats.effect.{Concurrent, ExitCode, IO, IOApp, Timer}
+import cats.effect.concurrent.Deferred
+import cats.effect.{Concurrent, ExitCode, Fiber, IO, IOApp, Timer}
+import cats.implicits._
+import cats.effect.implicits._
 import fs2.Stream
+import fs2.concurrent.SignallingRef
 import io.kirill.hellocats.utils.printing._
 
 import scala.concurrent.duration._
 
 object IdleTimeout extends IOApp {
 
-  def idleTimeout[F[_]: Timer, A](
-      s: fs2.Stream[F, A],
-      timeout: FiniteDuration
-  )(implicit F: Concurrent[F]): fs2.Stream[F, A] =
-    s
-      .groupWithin(1, timeout)
-      .evalMap(_.head.fold[F[A]](F.raiseError(new Exception("timeout")))(F.pure))
+  implicit final private class StreamOps[F[_], A](
+      private val stream: Stream[F, A]
+  ) {
 
-  override def run(args: List[String]): IO[ExitCode] = {
-    val s = Stream
+    def idleTimeout(timeout: FiniteDuration)(implicit T: Timer[F], F: Concurrent[F]): Stream[F, A] = {
+      def timeoutTimer(stopSignal: Deferred[F, Either[Throwable, Unit]]): F[Unit] =
+        T.sleep(timeout) *> stopSignal.complete(Left(new RuntimeException("timeout")))
+
+      (for {
+        stopSignal <- Stream.eval(Deferred[F, Either[Throwable, Unit]])
+        startTimer <- Stream.eval(timeoutTimer(stopSignal).start)
+        res <- stream
+          .evalScan[F, (Fiber[F, Unit], Option[A])]((startTimer, None)) { case ((to, _), el) =>
+            to.cancel *> timeoutTimer(stopSignal).start.map(t => (t, Some(el)))
+          }
+          .interruptWhen(stopSignal)
+      } yield res).map(_._2).unNone
+    }
+  }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    Stream
       .range[IO](0, 100)
       .evalTap(i => putStr[IO](i.toString))
       .metered(1.second)
-    idleTimeout[IO, Int](s, 100.millis)
+      .idleTimeout(500.millis)
       .handleErrorWith(e => Stream.eval_(putStr[IO](e.getMessage)))
       .compile
       .drain
       .as(ExitCode.Success)
-  }
 }
