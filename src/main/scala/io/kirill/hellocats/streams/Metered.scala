9a670df0388d5929d27e812546e03f91af443670
kirill5k <immotional@aol.com>
2021-02-10 09:50:51 +0000

    fix-rate immediate stream

--- src/main/scala/io/kirill/hellocats/streams/Metered.scala
+++ src/main/scala/io/kirill/hellocats/streams/Metered.scala
@@ -1,14 +1,25 @@
 package io.kirill.hellocats.streams
 
 import java.time.LocalTime
-
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Timer}
 import fs2.Stream
 import io.kirill.hellocats.utils.printing._
 
 import scala.concurrent.duration._
 
 object Metered extends IOApp {
+
+  def fixedRateImmediate[F[_]](d: FiniteDuration)(implicit timer: Timer[F]): Stream[F, Unit] = {
+    def now: Stream[F, Long] = Stream.eval(timer.clock.monotonic(NANOSECONDS))
+    def go(started: Long): Stream[F, Unit] =
+      now.flatMap { finished =>
+        val elapsed = finished - started
+        Stream.emit(()) ++ Stream.sleep_(d - elapsed.nanos) ++ now.flatMap { started =>
+          go(started)
+        }
+      }
+    now.flatMap(go)
+  }
 
   val timeStream = Stream
     .repeatEval(putStr[IO](LocalTime.now))
@@ -21,10 +32,11 @@ object Metered extends IOApp {
 
   val anotherImmediateStream = Stream
     .repeatEval(IO(LocalTime.now))
-    .zipLeft(Stream.awakeEvery[IO](1.second))
+    .zipLeft(fixedRateImmediate[IO](1.second))
     .evalTap(t => putStr[IO](s"$t - tapping"))
 
   override def run(args: List[String]): IO[ExitCode] =
-   putStr[IO](s"${LocalTime.now()} starting stream") *> anotherImmediateStream.compile.drain.as(ExitCode.Success)
+   putStr[IO](s"${LocalTime.now()} starting stream") *>
+     anotherImmediateStream.compile.drain.as(ExitCode.Success)
 
 }
