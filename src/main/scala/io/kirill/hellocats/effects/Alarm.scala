6ba13263edf8f7635ade1ffc032d7e2108859f2f
kirill5k <immotional@aol.com>
2021-03-07 19:44:41 +0000

    buzzing alarm

---
+++ src/main/scala/io/kirill/hellocats/effects/Alarm.scala
@@ -0,0 +1,60 @@
+package io.kirill.hellocats.effects
+
+import cats.effect.implicits._
+import cats.effect.concurrent.{Deferred, Ref}
+import cats.effect.{Concurrent, Fiber, Timer}
+import cats.implicits._
+
+import scala.concurrent.duration._
+
+/**
+ * Resettable timeout
+ */
+trait Alarm[F[_]] {
+  /**
+   * Sets the alarm `d` into the future
+   * Cancels the last reset
+   */
+  def reset(d: FiniteDuration): F[Unit]
+
+  /**
+   *  Completes `d` after `reset`. Never completes if called before `reset`
+   *  The purpose of `buzz` is to be raced with something else.
+   */
+  def buzz: F[Unit]
+
+  /**
+   * Cancels the last reset. `buzz` will never complete.
+   */
+  def cancel: F[Unit]
+}
+object Alarm {
+  def create[F[_]](implicit F: Concurrent[F], timer: Timer[F]): F[Alarm[F]] =
+    for {
+      empty <- Deferred[F, Fiber[F, Unit]]
+      state <- Ref[F].of(empty)
+      fb <- F.never[Unit].start
+      _ <- empty.complete(fb)
+    } yield new Alarm[F] {
+
+      def reset(d: FiniteDuration): F[Unit] = for {
+        newFiber <- Deferred[F, Fiber[F, Unit]]
+        oldFiber <- state.getAndSet(newFiber)
+        _ <- oldFiber.get.flatMap(_.cancel)
+        fiber <- timer.sleep(d).start
+        _ <- newFiber.complete(fiber)
+      } yield ()
+
+      def buzz: F[Unit] = for {
+        d <- state.get
+        fb <- d.get
+        _ <- fb.join
+      } yield ()
+
+      def cancel: F[Unit] = for {
+        d <- state.get
+        fb <- d.get
+        _ <- fb.cancel
+      } yield ()
+    }
+}
